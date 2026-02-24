ad1d3ae7962e6d30beae370010796e2ef2d74af9
MatinMammadli <metinmemmedlicontact@gmail.com>
2026-02-24 15:36:15 +0100

    Promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,20 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
-loadProducts(() => {
+import { loadCart } from "../data/cart.js";
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
